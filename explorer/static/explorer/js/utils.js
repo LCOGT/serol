80c5e1a954246be54f5907339a1ee050bce7332e
Edward Gomez <edward@gomez.me.uk>
2017-08-16 17:37:35 +0100

    Progress on identify

--- explorer/static/explorer/js/utils.js
+++ explorer/static/explorer/js/utils.js
@@ -58,6 +58,27 @@ var DateDiff = {
     }
 }
 
+function shuffle(array) {
+  /*
+  Fisher-Yates shuffle algorithm
+  */
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
 function status_request(requestid, token) {
   var data;
   console.log(token)
