4c333ba49950ac791c6437a1e517b65b3c67f8bf
Edward Gomez <edward@gomez.me.uk>
2018-07-13 14:07:10 +0100

    Allow authenticated users to add level scores

--- app/highscore/views.py
+++ app/highscore/views.py
@@ -1,4 +1,6 @@
 from django.utils.translation import ugettext as _
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Sum
 from profanityfilter import ProfanityFilter
 from rest_framework import serializers, generics, viewsets, status
 from rest_framework_jsonp.renderers import JSONPRenderer
@@ -19,15 +21,16 @@ class ScoreSerializer(serializers.ModelSerializer):
         fields = ('score', 'username')
 
     def validate_username(self, value):
-        """
-        Check that the username doesn't have naughty words in
-        """
-        f = ProfanityFilter()
-        if not f.is_clean(value):
-            logger.error('Username contained a swear')
-            raise serializers.ValidationError(_("Profane words are forbidden"))
-        return value
+        return check_username(value)
 
+class LevelScoreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = LevelScore
+        fields = ('score', 'username','level', 'user')
+
+    def validate_username(self, value):
+        return check_username(value)
 
 class HighScoreView(generics.ListAPIView):
     """
@@ -44,9 +47,46 @@ class AddHighScoreView(APIView):
     renderer_classes = (JSONRenderer, BrowsableAPIRenderer, JSONPRenderer)
     permission_classes = (IsAuthenticated,)
     def post(self, request, format=None):
-        ser = ScoreSerializer(data=request.data)
+        if request.user.username == 'game':
+            anonymous = True
+            ser = ScoreSerializer(data=request.data)
+        else:
+            anonymous = False
+            data = request.data
+            data['user'] = request.user.id
+            try:
+                oldscore = LevelScore.objects.get(user=request.user,level=data['level'])
+                ser = LevelScoreSerializer(oldscore, data=data)
+            except ObjectDoesNotExist:
+                print(data)
+                ser = LevelScoreSerializer(data=data)
+
         if not ser.is_valid(raise_exception=True):
             return Response(ser.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:
             ser.save()
+            if not anonymous:
+                auth_user_high_score(user=request.user)
             return Response("Success", status=status.HTTP_201_CREATED)
+
+def auth_user_high_score(user):
+    scores = LevelScore.objects.filter(user=user)
+    if scores.count() == 0:
+        return False
+
+    topscore = scores.aggregate(sum=Sum('score'))
+    score = Score()
+    score.score = topscore['sum']
+    score.username = scores[0].username
+    score.save()
+    return True
+
+def check_username(value):
+    """
+    Check that the username doesn't have naughty words in
+    """
+    f = ProfanityFilter()
+    if not f.is_clean(value):
+        logger.error('Username contained a swear')
+        raise serializers.ValidationError(_("Profane words are forbidden"))
+    return value
