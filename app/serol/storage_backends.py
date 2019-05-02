ac56bab94cfac377963c55e7387f3a42ee7f4fb6
Ira W. Snyder <isnyder@lco.global>
2019-05-02 16:42:27 -0700

    Make static files public by default
    The Internet should be able to read static files without needing an
    permissions. The Internet should not be able to read media files without
    being granted permission.

--- app/serol/storage_backends.py
+++ app/serol/storage_backends.py
@@ -1,12 +1,12 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 from django.conf import settings
 
-
+# Static files are public (anyone can read them)
 class StaticStorage(S3Boto3Storage):
     location = 'static'
-    default_acl = None
+    default_acl = 'public-read'
 
-
+# Media files are not public. Only authorized users should have access.
 class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = None
