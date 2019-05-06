d18b728b6dba2e1e22ae47ffe2681a144e4df191
Ira W. Snyder <isnyder@lco.global>
2019-05-06 16:01:49 -0700

    Force AWS S3 V4 Signatures
    AWS S3 will stop accepting V2 signatures on 2019-06-24. AWS S3 V4
    signatures should be used in their place.
    
    https://forums.aws.amazon.com/ann.jspa?annID=5816

--- app/serol/settings.py
+++ app/serol/settings.py
@@ -151,6 +151,7 @@ if str2bool(os.getenv('USE_S3', 'False')):
     AWS_S3_REGION_NAME = os.getenv('AWS_DEFAULT_REGION', 'us-west-2')
     AWS_DEFAULT_ACL = None
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_S3_SIGNATURE_VERSION = 's3v4'
     # s3 public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://s3-{AWS_S3_REGION_NAME}.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{PUBLIC_MEDIA_LOCATION}/'
