742acfb2daac2ddfc79deb4433d7f06a5cc6a0de
Edward Gomez <edward@gomez.me.uk>
2020-09-21 13:47:49 +0100

    Fixing gevent bad version

--- app/serol/settings.py
+++ app/serol/settings.py
@@ -143,7 +143,7 @@ if ast.literal_eval(os.environ.get('USE_S3', 'False')):
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME', ''),
         'USER': os.environ.get('DB_USER',''),
         'PASSWORD': os.environ.get('DB_PASS',''),
