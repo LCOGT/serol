2bbd85ba799b102e8376de1ffda9217fc74f22d9
Edward Gomez <edward@gomez.me.uk>
2023-07-19 12:13:57 +0100

    Adding serol background to case-study

--- explorer/views.py
+++ explorer/views.py
@@ -113,7 +113,8 @@ class ChallengeView(ChallengeBaseView):
         self.object = self.get_object()
         context = self.get_context_data(object=self.object)
         user, readonly = get_current_user(self.request)
-        print(user, readonly)
+        if not user:
+            return HttpResponseRedirect(reverse('auth_login'))
         context['user'] = user
         context['readonly'] = readonly
         context['icon'] = target_icon(self.object.avm_code)
